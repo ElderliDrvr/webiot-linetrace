54b97fa05a4c5627af2698e952a0443450cdd5cc
grande <87326435+ElderliDrvr@users.noreply.github.com>
2024-02-11 20:00:30 +0900

    Update linetrace_test.py

--- linetrace_test.py
+++ linetrace_test.py
@@ -4,30 +4,30 @@ import time
 # GPIOピンの設定
 GPIO.setmode(GPIO.BOARD)
 
-SENSOR1 = 11
-SENSOR2 = 13
+SENSOR1 = 19
+SENSOR2 = 26
 GPIO.setup(SENSOR1, GPIO.IN)  # IRセンサー1の入力ピン
 GPIO.setup(SENSOR2, GPIO.IN)  # IRセンサー2の入力ピン
 
 def linetrace():
     while True:
         if GPIO.input(SENSOR1) == GPIO.LOW and GPIO.input(SENSOR2) == GPIO.LOW:
-            # 両方のセンサーでラインが検出された場合、適切な処理を行う
+            # 直進
             print("両方のセンサーでラインが検出されました")
             # ロボットの動作を制御するためのコードを追加する
 
         elif GPIO.input(SENSOR1) == GPIO.LOW:
-            # センサー1でラインが検出された場合、適切な処理を行う
+            # 左右のどちらかに旋回
             print("センサー1でラインが検出されました")
             # ロボットの動作を制御するためのコードを追加する
 
         elif GPIO.input(SENSOR2) == GPIO.LOW:
-            # センサー2でラインが検出された場合、適切な処理を行う
+            # 左右のどちらかに旋回
             print("センサー2でラインが検出されました")
             # ロボットの動作を制御するためのコードを追加する
 
         else:
-            # ラインが検出されなかった場合、適切な処理を行う
+            # 停止 手動操縦でラインまで復帰できるようにするのもアリ
             print("ラインが検出されませんでした")
             # ロボットの動作を制御するためのコードを追加する
 
